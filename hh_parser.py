b8582f40103d3165fa1f268c38b2ff26a43a8044
Valentine <42617437+valentinepit@users.noreply.github.com>
2021-08-31 23:01:01 +0500

    Lesson_3 Lesson_4
    Не успел с третьим уроком чутка ))

--- hh_parser.py
+++ hh_parser.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from pymongo import MongoClient
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -9,24 +10,43 @@ params = {'fromSearchLine': 'true', 'st': 'searchVacancy', 'text': 'python', 'pa
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
 
+client = MongoClient('localhost', 27017)
+db = client['head_hunter']
+p_vc = db.python_vacancies
 
-def clear(_payment):
+# можно конечно взять и с какого-нибудь API,
+usd = 73.51
+kzt = 0.17
+
+
+def convertation(data, cur):
     money = {}
-    _payment = _payment.replace('\u202f', '').split()
-    money['currency'] = _payment[-1]
-
-    if 'от' in _payment:
-        money['from'] = int(_payment[1])
+    if 'от' in data:
+        money['from'] = int(data[1]) * cur
         money['to'] = None
-    elif 'до' in _payment:
+    elif 'до' in data:
         money['from'] = None
-        money['to'] = int(_payment[1])
-    elif '–' in _payment:
-        money['from'] = int(_payment[0])
-        money['to'] = int(_payment[2])
-
+        money['to'] = int(data[1]) * cur
+    elif '–' in data:
+        money['from'] = int(data[0]) * cur
+        money['to'] = int(data[2]) * cur
     return money
 
+def clear(_payment):
+    _payment = _payment.replace('\u202f', '').split()
+    money = {}
+    if _payment[-1] == 'KZT':
+        money = convertation(_payment, kzt)
+    elif _payment[-1] == 'USD':
+        money = convertation(_payment, usd)
+    elif _payment[-1] == 'руб.':
+        money = convertation(_payment, 1)
+    money['currency'] = 'руб.'
+    return money
+
+
+def mongo_insert(_vacancy):
+    p_vc.update_one(_vacancy, {'$set': _vacancy}, upsert=True)
 
 def preparation(_soup):
     vacancies = []
@@ -41,7 +61,7 @@ def preparation(_soup):
             payment = {'from': None, 'to': None, 'currency': None}
         vacancy['title'] = title
         vacancy['money'] = payment
-        # print(vacancy)
+        mongo_insert(vacancy)
         vacancies.append(vacancy)
     return vacancies
 
@@ -49,6 +69,21 @@ def preparation(_soup):
 def list_count(_soup):
     pg_buttons = _soup.findAll('a', {'data-qa': 'pager-page'})
     return int(pg_buttons[-1].text)
+
+
+def find_money():
+    value = int(input('Введите минимальный размер оплаты '))
+    for item in p_vc.find({'money.currency': 'руб.'}):
+        try:
+            if item.get('money').get('from') >= value:
+                pprint(item)
+                continue
+        except:
+            try:
+                if item.get('money').get('to') >= value:
+                    pprint(item)
+            except:
+                continue
 
 
 def main():
@@ -61,10 +96,10 @@ def main():
         for page_number in range(1, max_lists):
             soup = bs(response.text, 'html.parser')
             job.append(preparation(soup))
-        pprint(job)
     else:
         print(f'{response}')
 
+    find_money()
 
 if __name__ == '__main__':
     main()
